d590713bf01c801864a93416bdacae6b13f129a8
locolome <locod4mw@gmail.com>
2022-05-03 21:52:14 -0300

    aula23

--- Aula23.py/ex3.py
+++ Aula23.py/ex3.py
@@ -1,8 +1,17 @@
-from random import randint
+from random import randrange
+n=int(input("Informe o valor para n: "))
+
+if n>0:
+    l1=[randrange(1,11) for i in range(n)]
+    l2=[randrange(1,11) for i in range(n)]
+    
+    l3=[]
+    
+    for i in range(n):
+        l3.append(l1[i]+l2[i])
+
+    print(f'L1 = {l1}')
+    print(f'L2 = {l2}')
+    print(f'L3 = {l3}')
 
 
-N=int(input('Digite um valor: '))
-l1=[]
-l2=[]
-l3=[]
-l1=randint
