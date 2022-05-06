6c4222993a7036502342f28da0d61b7ef6c806f0
locolome <locod4mw@gmail.com>
2022-05-05 21:50:05 -0300

    aa

--- Aula26.py/ex4.py
+++ Aula26.py/ex4.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 
-def sorteia():
+'''def sorteia():
     numeros=[]
     a=0
     for c in range(5):
@@ -19,4 +19,20 @@ def somapar(numeros):
             soma=soma+numeros
     return soma
     
-print(somapar(sorteia()))
+print(somapar(sorteia()))'''
+
+def sorteia(lista):
+    for cont in range(0,5):
+        n=randint(1,10)
+        lista.append(n)
+
+def somapar(lista):
+    soma=0
+    for valor in lista:
+        if valor%2==0:
+            soma+=valor
+    print(f'Somando os valores pares de {lista} temos {soma}')
+
+numeros=list()
+sorteia(numeros)
+somapar(numeros)
